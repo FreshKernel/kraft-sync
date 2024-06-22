f19e1d92b307f2ef3258ca5a4e5be199821f8074
Ellet <ellet@freshplatform.net>
2024-06-22 23:53:34 +0300

    chore: update incorrect comment in ModrinthLauncherDataSource

--- admin/src/main/kotlin/launchers/modrinth/ModrinthLauncherDataSource.kt
+++ admin/src/main/kotlin/launchers/modrinth/ModrinthLauncherDataSource.kt
@@ -9,39 +9,35 @@ import java.nio.file.Paths
 class ModrinthLauncherDataSource : LauncherDataSource {
     companion object {
         /**
-         * ATLauncher will create a file in the instance directory containing all the info for the instance
+         * Modrinth Launcher will create a file in the instance directory containing all the info for the instance
          * */
         const val INSTANCE_FILE_NAME = "profile.json"
     }
 
-    private fun getInstance(launcherInstanceDirectory: File): Result<ModrinthLauncherInstance> {
-        return try {
+    private fun getInstance(launcherInstanceDirectory: File): Result<ModrinthLauncherInstance> =
+        try {
             val instanceFile = Paths.get(launcherInstanceDirectory.path, INSTANCE_FILE_NAME).toFile()
             val modrinthLauncherInstance = JsonIgnoreUnknownKeys.decodeFromString<ModrinthLauncherInstance>(instanceFile.readText())
             Result.success(modrinthLauncherInstance)
         } catch (e: Exception) {
             Result.failure(e)
         }
-    }
 
-    override suspend fun isValidInstanceDirectory(launcherInstanceDirectory: File): Result<Unit> {
-        return try {
+    override suspend fun isValidInstanceDirectory(launcherInstanceDirectory: File): Result<Unit> =
+        try {
             getInstance(launcherInstanceDirectory = launcherInstanceDirectory).getOrThrow()
             Result.success(Unit)
         } catch (e: Exception) {
             Result.failure(e)
         }
-    }
 
-    override suspend fun isCurseForgeApiRequestNeeded(launcherInstanceDirectory: File): Result<Boolean> {
-        return Result.success(false)
-    }
+    override suspend fun isCurseForgeApiRequestNeeded(launcherInstanceDirectory: File): Result<Boolean> = Result.success(false)
 
     override suspend fun getMods(
         launcherInstanceDirectory: File,
         overrideCurseForgeApiKey: String?,
-    ): Result<List<Mod>> {
-        return try {
+    ): Result<List<Mod>> =
+        try {
             val instance = getInstance(launcherInstanceDirectory = launcherInstanceDirectory).getOrThrow()
             val mods =
                 instance.projects.map { (_, project) ->
@@ -61,5 +57,4 @@ class ModrinthLauncherDataSource : LauncherDataSource {
         } catch (e: Exception) {
             Result.failure(e)
         }
-    }
 }
